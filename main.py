0e68c628a2acde2f7b853f7c763d283f12972b47
zheng <2642198391@qq.com>
2022-09-29 18:59:20 +0800

    加入-f参数，用于选择功能

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 
 from create_questions import create_questions
 from calculation import  calculation
+from compare import compare
 def main_func():
     parser =argparse.ArgumentParser()
     # 控制生成题目的个数
@@ -13,15 +14,22 @@ def main_func():
     parser.add_argument('-e', default='exercisefile.txt',help='将生成的题目输出到对应文件')
     # 将统计结果输出到对应文件
     parser.add_argument('-a', default='answerfile.txt',help='将统计结果输出到对应文件')
+    # 设置功能参数，是生成题目和答案还是对比答案
+    parser.add_argument('-f',type=int,default=1,help='设置功能参数')
     # 获取参数
     args = parser.parse_args()
     # 打印参数
     # print(args.r)
     # 创建生成问题的类，传入生成的个数和范围
-    create_questions1 = create_questions(args.n,args.r)
-    create_questions1.main_work()
-    calculation1 = calculation()
-    calculation1.dealwith_singleline()
+    if args.f == 1:
+        create_questions1 = create_questions(args.n,args.r)
+        create_questions1.main_work()
+        calculation1 = calculation()
+        calculation1.dealwith_singleline()
+    elif args.f == 2:
+        compare1 = compare()
+        compare1.compare_correct()
+        compare1.write_info()
 
 if __name__ == '__main__':
     main_func()
